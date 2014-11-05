56ad42c654b03461fb1b960356692c35945f2134
Ivan Klarman <klarman.ivan@gmail.com>
2014-11-05 22:12:49 +0100

    Implémentation de la méthode IsValid
    Test de la première ligne et première colonne qu'avec le caractère '*'
    pour la formation du mur.
    Si vous avez des idées pour la dernière ligne et dernière colonne.
    N'hésitez pas je bug dessus.

--- SimulateurFoule/src/Map.java
+++ SimulateurFoule/src/Map.java
@@ -4,7 +4,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 public class Map {
 	public char[][] mapTab = new char[255][255];
-	
+	private boolean isValid = true;
+	/*
+	 * Constructeur de la map
+	 */
 	public Map(String file){
 		String chaine="";
 		try{
@@ -15,9 +18,24 @@ public class Map {
 			int cpLine = 0;
 			int cpCol = 0;
 			while ((ligne=br.readLine())!=null){
-				char[] lineChar = ligne.toCharArray();
+				char[] lineChar = ligne.toCharArray();	
 				for(int i=0;i<lineChar.length;i++){
 					mapTab[cpLine][cpCol] = lineChar[i];
+					// Vérification ligne de mur
+					if(mapTab[0][cpCol] != '*'){
+						isValid = false;
+						break;
+					}
+					if(mapTab[cpLine][0] != '*'){
+						isValid = false;
+						break;
+					}
+					if(cpLine == 0){
+						if(lineChar[i] != '*' || lineChar[lineChar.length-1] != '*'){
+							isValid = false;
+							break;
+						}
+					}
 					System.out.print(mapTab[cpLine][cpCol]);
 					cpCol++;
 				}
@@ -26,7 +44,9 @@ public class Map {
 				cpLine++;
 				chaine+=ligne;
 			}
-			System.out.println("Initialisation de la Map - OK");
+			if(this.isValid()){
+				System.out.println("Initialisation de la Map - OK");
+			}
 			br.close(); 
 		}		
 		catch (Exception e){
@@ -34,10 +54,19 @@ public class Map {
 			System.out.println(e.toString());
 		}
 	}
+	/*
+	 * Récupérer la map
+	 */
 	public char[][] getMapTab(){
 		return mapTab;
 	}
+	/*
+	 * Créer une nouvelle Map
+	 */
 	public void setMapTab(String file){
 		new Map(file);
+	}
+	public boolean isValid(){
+		return isValid;
 	}
 }
