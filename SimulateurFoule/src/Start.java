16bf46e2b583e4da0320e026bd748005f64030e4
Ivan Klarman <klarman.ivan@gmail.com>
2014-11-24 10:24:05 +0100

    Changement du Main pour test
    Changement Main pour Test

--- SimulateurFoule/src/Start.java
+++ SimulateurFoule/src/Start.java
@@ -3,8 +3,7 @@ public class Start {
 	public static void main(String[] args) {
 		String fichier = "src\\ressource\\map.txt";
 		Map oMap = new Map(fichier);
-		System.out.println(oMap.totalCaractere);
-		System.out.println("Nombre Caractere ligne : "+oMap.getLineNumber());
-		oMap.test();
+		System.out.println("Nombre Colonnes : "+ oMap.getNbColonne());
+		System.out.println("Nombre Lignes : "+ oMap.getNbLigne(fichier));
 	}
 }
